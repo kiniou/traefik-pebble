8c5799d306238bc94a9c9ee45deed458005a7e63
Kevin Roy <kiniou@gmail.com>
2019-06-23 18:47:39 +0200

    rework network ports publishing
    - added ipv6 local ports.
    - renamed docker network to `traefik`.
    - added a quick script to create the network.
    - simplified traefik.service and added a pull/build to refresh docker images.

---
+++ scripts/create-traefik-network.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+set -e
+
+loadenv() {
+    if [ -f .env ]; then
+        while read -r a; do
+            export "${a?}"
+        done < .env
+    fi
+}
+
+loadenv
+
+NETWORK_NAME="${NETWORK_NAME:-traefik}"
+
+# Remove network if it exists
+docker network ls --format="{{.Name}}" | grep -E "^${NETWORK_NAME}$" \
+    && docker network rm "${NETWORK_NAME}"
+
+# Create a fresh new network
+docker network create \
+       --attachable \
+       -o 'com.docker.network.bridge.name'='${NETWORK_NAME}' \
+       "${NETWORK_NAME}"
